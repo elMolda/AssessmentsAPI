c7db0ac2a21415daae9ca980499f3c09485e8ba9
elMolda <david97molano@gmail.com>
2019-11-27 18:40:22 -0500

    Test endpoint manager created

--- Code/app.py
+++ Code/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__) 
 
 @app.route('/ping', methods = ['GET'])
 def ping():
     return jsonify({"message": 'Pong'})
+
+@app.route('/test', methods = ['GET'])
+def manageTestEndpoint():
+    if request.method == 'GET': #Main endpoint of API
+        return jsonify({"message": """This is Test endpoint.Send POST request to this with user parameters to start assessment."""})
 
 if __name__ == '__main__': 
     app.run(debug=True, port=5000)
