0dad981cea03619b57038c0b2d50d26eed079d85
elMolda <david97molano@gmail.com>
2019-11-28 12:05:32 -0500

    Email validation done. Pending documentation

--- Code/app.py
+++ Code/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, make_response
 from Controllers import prepareAssessment
 from Entities.Assessment import Assessment
+from Utils import checkEmail
 import sqlite3
 app = Flask(__name__) 
 
@@ -12,10 +13,13 @@ def ping():
 def manageTestEndpoint():
     response = None
     if request.method == 'GET': #Main endpoint of API
-        response = jsonify({"message": """This is Test endpoint.Send POST request to this with user parameters to start assessment."""})
+        response = jsonify({"Message": """This is Test endpoint.Send POST request to this with user parameters to start assessment."""})
     if request.method == 'POST': #Create a new User if needed. Create new Assemnt.
-        asst = prepareAssessment.prepareAssessment(request)
-        response = make_response(jsonify({"Assessment created":asst.asstToJson()}),201)
+        if checkEmail.checkEmail(request.json['email']):
+            asst = prepareAssessment.prepareAssessment(request)
+            response = make_response(jsonify({"Assessment created":asst.asstToJson()}),201)
+        else:
+            response = make_response(jsonify({"Message":"Not valid Email"}),400)
     return response
 if __name__ == '__main__': 
     app.run(debug=True, port=5000)
