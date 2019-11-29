37ea649772e5a760c65ebf779dbd7a080cfd8a61
elMolda <david97molano@gmail.com>
2019-11-29 11:43:25 -0500

    Doc to ansQuestion

---
+++ Code/Controllers/answerQuestion.py
@@ -0,0 +1,9 @@
+from flask import request
+from DatabaseAccess import updateAssmntClosedQuestion, updateAssmntOpenQuestion
+
+def answerQuestion(assessment_key,question_n,question_type,request):
+    #Validate the question type
+    if question_type == 0: #Question is closed
+        updateAssmntClosedQuestion.updateAssmntClosedQuestion(assessment_key,question_n,request.json['selectedAnswer']) #Call propper controller  with selectedAnswer from request
+    elif question_type == 1: #Question is open
+        updateAssmntOpenQuestion.updateAssmntOpenQuestion(assessment_key,question_n,request.json['answerBody']) #Call propper controller  with answerBody from request
