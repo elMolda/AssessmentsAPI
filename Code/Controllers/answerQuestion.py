789113ce02e6e581730f7a9adfd9aacafb099792
elMolda <david97molano@gmail.com>
2019-11-29 11:08:29 -0500

    Done answerign open questions. Pending documentation

--- Code/Controllers/answerQuestion.py
+++ Code/Controllers/answerQuestion.py
@@ -1,9 +1,8 @@
 from flask import request
-from DatabaseAccess import updateAssmntClosedQuestion
+from DatabaseAccess import updateAssmntClosedQuestion, updateAssmntOpenQuestion
 
 def answerQuestion(assessment_key,question_n,question_type,request):
     if question_type == 0: #Question is closed
         updateAssmntClosedQuestion.updateAssmntClosedQuestion(assessment_key,question_n,request.json['selectedAnswer'])
     elif question_type == 1: #Question is open
-        print("OK")
-        #updateAssmntOpenQuestion.updateAssmntOpenQuestion(assessment_key,question_n,request.json['answerBody'])
+        updateAssmntOpenQuestion.updateAssmntOpenQuestion(assessment_key,question_n,request.json['answerBody'])
