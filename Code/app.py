936002095746b9997eb872c7b3a6cde4cc8b2f06
elMolda <david97molano@gmail.com>
2019-12-02 20:31:11 -0500

    Refactored logic of last question

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request, make_response, redirect #Flask modules used
-from Controllers import prepareAssessment, showQuestion, answerQuestion, getAsstTimes, getAllQuestions, answerLastQuestion #Files from Controllers package
+from flask import Flask, jsonify, request, make_response #Flask modules used
+from Controllers import prepareAssessment, showQuestion, answerQuestion, getAsstTimes, getAllQuestions #Files from Controllers package
 from Entities.Assessment import Assessment #Files from Entities package
 from Utils import checkEmail, checkDate #Files from Utils package
 import datetime
@@ -42,12 +42,7 @@ def answerOneQuestion(assessment_key,question_n):
         sentTime = datetime.datetime.now() #Get the time when request was made
         if checkDate.checkDate(sentTime, startTime, deadlineTime): #If the request was made in appropiate time answer
             question_type = showQuestion.showQuestion(assessment_key,question_n).isOpen #Get question type to set apropiate behavior
-            if question_n == 6:#Is last question
-                asst = answerLastQuestion.answerLastQuestion(assessment_key,question_type,request,sentTime)#Answer question and getAssessment data to confirm finished
-                response = make_response(jsonify({"Assessment finished":asst.asstToJson()}),200)
-            else: #Not last question
-                answerQuestion.answerQuestion(assessment_key,question_n,question_type,request) #Answer question 
-                response = make_response(jsonify({"Answered Recieved": "OK"}), 201)
+            response = answerQuestion.answerQuestion(assessment_key,question_n,question_type,request,sentTime) #Answer question 
         else:
             response = make_response(jsonify({"Out of Time": "FAILED"}), 401) #Resquest was made out of time
     return response
